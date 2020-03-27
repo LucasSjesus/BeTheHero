6ad9523f08c8c7b43cd1b27046930f2d920f59dd
Lucas Souza de Jesus <lucassjesus@hotmail.com>
2020-03-27 18:11:55 -0300

    Atualização do backend com o test server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require ('cors')
+const {errors} = require ('celebrate')
 const routes = require('./routes')
+
 
 const app = express();
 
@@ -8,6 +10,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(routes)
+app.use(errors())
+module.exports = app
 
 /**
  * Rota / Recurso
@@ -42,4 +46,4 @@ app.use(routes)
 
 
 
-app.listen(3333);
+
--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto')
+const generateUniqueId = require('../utils/generateUniqueId')
 const connection = require ('../database/connection')
 
 module.exports = {
@@ -16,7 +16,7 @@ module.exports = {
             const {name, email, whatsapp, city, uf} = request.body
                 
         
-            const id = crypto.randomBytes(4).toString('HEX')
+            const id = generateUniqueId();
         
             await connection('ongs').insert({
                 id,
